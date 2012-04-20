31fcbad2133a09b918130b7f842240ccff148b2f
Jens Burkhardt <jensburkhardt@web.de>
2012-04-20 20:55:09 +0200

    much better

--- src/com/tipgame/ui/KickOffView.java
+++ src/com/tipgame/ui/KickOffView.java
@@ -20,6 +20,10 @@ public class KickOffView extends CustomComponent {
 
 	/*- VaadinEditorProperties={"grid":"RegularGrid,20","showGrid":true,"snapToGrid":true,"snapToObject":true,"movingGuides":false,"snappingDistance":10} */
 
+	
+
+	/*- VaadinEditorProperties={"grid":"RegularGrid,20","showGrid":true,"snapToGrid":true,"snapToObject":true,"movingGuides":false,"snappingDistance":10} */
+
 	private static final long serialVersionUID = 8579726314990507664L;
 	
 	
@@ -56,19 +60,20 @@ public class KickOffView extends CustomComponent {
 		// common part: create layout
 		mainLayout = new VerticalLayout();
 		mainLayout.setImmediate(false);
-		mainLayout.setWidth("180px");
-		mainLayout.setHeight("-1px");
+		mainLayout.setWidth("140px");
+		mainLayout.setHeight("60px");
 		mainLayout.setMargin(false);
 		
 		// top-level component properties
-		setWidth("180px");
-		setHeight("-1px");
+		setWidth("140px");
+		setHeight("60px");
 		
 		// LabelKickOff
 		LabelKickOff = new Label();
 		LabelKickOff.setImmediate(false);
 		LabelKickOff.setWidth("-1px");
 		LabelKickOff.setHeight("-1px");
+		LabelKickOff.setValue("01.01.2012 10:00 Uhr");
 		mainLayout.addComponent(LabelKickOff);
 		mainLayout.setComponentAlignment(LabelKickOff, new Alignment(48));
 		
